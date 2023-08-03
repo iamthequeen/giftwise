2418a048d2da38a58625c44fd60f2b86798f172a
Bianca Simon <biancasimon2112@gmail.com>
2023-08-02 20:46:28 -0500

    update format of App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { useContext, useEffect } from "react";
-import { Box, Button, ButtonGroup, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
 import { UserContext } from "./utils/UserContext";
 import { auth } from "./utils/firebaseSetup";
 import LoadingScreen from "./components/ui/LoadingScreen/LoadingScreen";
-import AboutMeList from "./pages/AboutMeList/AboutMeList"
-import LogoutPage from "./pages/routes/LogoutPage/LogoutPage"
-import LandingPage from "./pages/routes/LandingPage/LandingPage"
-import ProfilePage from "./pages/routes/ProfilePage/ProfilePage"
-import { Route, Routes } from 'react-router-dom'
+import AboutMeList from "./pages/AboutMeList/AboutMeList";
+import LogoutPage from "./pages/routes/LogoutPage/LogoutPage";
+import LandingPage from "./pages/routes/LandingPage/LandingPage";
+import ProfilePage from "./pages/routes/ProfilePage/ProfilePage";
+import { Route, Routes } from "react-router-dom";
 import LoginForm from "./components/LoginForm/LoginForm";
 import SignupForm from "./components/SignupForm/SignupForm";
 import ErrorPage from "./pages/routes/ErrorPage/ErrorPage";
@@ -15,26 +22,21 @@ import SignupProcess from "./pages/routes/SignupProcess/SignupProcess";
 import ProtectLogout from "./pages/routes/ProtectLogout/ProtectLogout";
 import ProtectedRoute from "./pages/routes/ProtectedRoute/ProtectedRoute";
 import ProtectSignup from "./pages/routes/ProtectSignup/ProtectSignup";
-import {useLocation} from 'react-router-dom'
+import { useLocation } from "react-router-dom";
 import UpdateProfile from "./pages/UpdateProfile/UpdateProfile";
 import ExplorePage from "./pages/routes/ExplorePage/ExplorePage";
 
-
 function App() {
+  const { currentUser, viewingUser } = useContext(UserContext);
 
-    const {
-        currentUser, viewingUser,
-    } = useContext(UserContext)
-
-
-    const theme = createTheme({
-         typography: {
+  const theme = createTheme({
+    typography: {
       button: {
         fontWeight: 400,
         textTransform: "none",
       },
     },
-        components: {
+    components: {
       MuiButton: {
         defaultProps: {
           variant: "contained",
@@ -56,15 +58,15 @@ function App() {
           },
         },
       },
-    //   MuiBottomNavigationAction: {
-    //     styleOverrides: {
-    //       root: {
-    //         '& .Mui-selected': {
-    //       color: '#655FB1',
-    //     },
-    //       },
-    //     },
-    //   },
+      //   MuiBottomNavigationAction: {
+      //     styleOverrides: {
+      //       root: {
+      //         '& .Mui-selected': {
+      //       color: '#655FB1',
+      //     },
+      //       },
+      //     },
+      //   },
       MuiListItemButton: {
         styleOverrides: {
           root: {
@@ -76,129 +78,127 @@ function App() {
         },
       },
     },
-        palette: {
-            mode: "light",
-            // giftwise palette:
-            lightGrayPrimary: {
-                main: "#e7e7e7",
-                contrastText: "#000"
-            },
-            oliveGreenPrimary: {
-                light: "#B3AD94",
-                main: "#837d5d",
-                dark: "#5F5A44",
-                contrastText: "#fff",
-            },
-            blackPrimary: {
-                main: "#000",
-                contrastText: "#FFF",
-            },
-            whitePrimary: {
-                main: "#FFF",
-                contrastText: "#000"
-            },
-           lightYellowPrimary: {
-                main: "#fefae0",
-                contrastText: "#000"
-            },
-            orangePrimary: {
-                light: "#ECD6C1",
-                main: "#D4A373",
-                dark: "#AC7035",
-                darker: "#8c5c28",
-                contrastText: "#000"
-            },
-            lightGreenPrimary: {
-                light: "#EEF1E4",
-                main: "#CCD5AE",
-                dark: "#A2B36B",
-                darker: "#8fa253",
-                contrastText: "#000",
-            },
-            aquaPrimary: {
-                light: "#90DEDF",
-                main: "#39C3C6",
-                dark: "#257D7E",
-                darker: "#1c5e5f",
-                contrastText: "#000",
-            },
-            lightBrownPrimary: {
-                light: "#DFD4CE",
-                main: "#B49A8C",
-                dark: "#886959",
-                darker: "#6f5649",
-                contrastText: "#000",
-            },
-            lightPinkPrimary: {
-                light: "#FEEDEB",
-                main: "#FCB9B2",
-                dark: "#F97162",
-                darker: "#F73B26",
-                contrastText: "#000"
-            },
-            // faithflow palette:
-            whiteSecondary: {
-                main: "#FFECE0",
-                contrastText: "#000"
-            },
-            bluePrimary: {
-                main: "rgba(186, 207, 255, 0.74)",
-                contrastText: "#000"
-            },
-            lightBluePrimary: {
-                main: "#A5C8E4",
-                contrastText: "#000"
-            },
-            darkBluePrimary: {
-                main: "#655FB1",
-                contrastText: "#fff"
-            },
-            darkBlueSecondary: {
-                main: "#0E133C",
-                light: "#26339e",
-                contrastText: "#fff"
-            },
-            darkGrayPrimary: {
-                main: "#454545",
-                contrastText: "#fff"
-            },
-            darkGraySecondary: {
-                main: "#787878",
-                dark: "#2f2f2f",
-                contrastText: "#fff"
-            },
-            whiteTertiary: {
-                main: "#F6F6F6",
-                contrastText: "#000"
-            },
-            lightBlueSecondary: {
-                dark: "#0E133C",
-                main: "#26339e",
-                contrastText: "#fff"
-            },
-            redPrimary: {
-                main: "#FF0000",
-                contrastText: "#000"
-            },
-            yellowPrimary: {
-                main: "#FFDF02",
-                contrastText: "#000",
-            },
-            beigePrimary: {
-              main: "#f4eed7",
-              contrastText: "#000",
-          },
-            text: {
-        primary:"#000"
+    palette: {
+      mode: "light",
+      // giftwise palette:
+      lightGrayPrimary: {
+        main: "#e7e7e7",
+        contrastText: "#000",
       },
-        }
-    })
-
-
+      oliveGreenPrimary: {
+        light: "#B3AD94",
+        main: "#837d5d",
+        dark: "#5F5A44",
+        contrastText: "#fff",
+      },
+      blackPrimary: {
+        main: "#000",
+        contrastText: "#FFF",
+      },
+      whitePrimary: {
+        main: "#FFF",
+        contrastText: "#000",
+      },
+      lightYellowPrimary: {
+        main: "#fefae0",
+        contrastText: "#000",
+      },
+      orangePrimary: {
+        light: "#ECD6C1",
+        main: "#D4A373",
+        dark: "#AC7035",
+        darker: "#8c5c28",
+        contrastText: "#000",
+      },
+      lightGreenPrimary: {
+        light: "#EEF1E4",
+        main: "#CCD5AE",
+        dark: "#A2B36B",
+        darker: "#8fa253",
+        contrastText: "#000",
+      },
+      aquaPrimary: {
+        light: "#90DEDF",
+        main: "#39C3C6",
+        dark: "#257D7E",
+        darker: "#1c5e5f",
+        contrastText: "#000",
+      },
+      lightBrownPrimary: {
+        light: "#DFD4CE",
+        main: "#B49A8C",
+        dark: "#886959",
+        darker: "#6f5649",
+        contrastText: "#000",
+      },
+      lightPinkPrimary: {
+        light: "#FEEDEB",
+        main: "#FCB9B2",
+        dark: "#F97162",
+        darker: "#F73B26",
+        contrastText: "#000",
+      },
+      // faithflow palette:
+      whiteSecondary: {
+        main: "#FFECE0",
+        contrastText: "#000",
+      },
+      bluePrimary: {
+        main: "rgba(186, 207, 255, 0.74)",
+        contrastText: "#000",
+      },
+      lightBluePrimary: {
+        main: "#A5C8E4",
+        contrastText: "#000",
+      },
+      darkBluePrimary: {
+        main: "#655FB1",
+        contrastText: "#fff",
+      },
+      darkBlueSecondary: {
+        main: "#0E133C",
+        light: "#26339e",
+        contrastText: "#fff",
+      },
+      darkGrayPrimary: {
+        main: "#454545",
+        contrastText: "#fff",
+      },
+      darkGraySecondary: {
+        main: "#787878",
+        dark: "#2f2f2f",
+        contrastText: "#fff",
+      },
+      whiteTertiary: {
+        main: "#F6F6F6",
+        contrastText: "#000",
+      },
+      lightBlueSecondary: {
+        dark: "#0E133C",
+        main: "#26339e",
+        contrastText: "#fff",
+      },
+      redPrimary: {
+        main: "#FF0000",
+        contrastText: "#000",
+      },
+      yellowPrimary: {
+        main: "#FFDF02",
+        contrastText: "#000",
+      },
+      beigePrimary: {
+        main: "#f4eed7",
+        contrastText: "#000",
+      },
+      text: {
+        primary: "#000",
+      },
+    },
+  });
 
   return (
     <ThemeProvider theme={theme}>
-     <CssBaseline>
+      <CssBaseline>
         <Box
           sx={{
             position: "absolute",
@@ -211,47 +211,61 @@ function App() {
             background: "#000",
           }}
         >
+          <Routes>
+            <Route exact={true} path="/" element={<LandingPage />} />
+            <Route
+              path="/explore"
+              element={
+                <ProtectedRoute>
+                  <ExplorePage />
+                </ProtectedRoute>
+              }
+            />
 
-  <Routes>
-         
+            <Route path="/login" element={<LoginForm />} />
 
-<Route exact={true} path="/" element={<LandingPage />}/> 
-             <Route path="/explore" element={
-             <ProtectedRoute>
-            <ExplorePage />
-            </ProtectedRoute>}/>       
-         
-         <Route path="/login" element={<LoginForm/>} />
+            <Route
+              path="/logout"
+              element={
+                <ProtectLogout>
+                  <LogoutPage />
+                </ProtectLogout>
+              }
+            />
 
-         <Route path="/logout" element={
-            <ProtectLogout>
-            <LogoutPage/>
-            </ProtectLogout>} />
+            <Route path="/createaccount" element={<SignupProcess />} />
 
-            <Route path="/createaccount" element={<SignupProcess/>} />
+            <Route
+              path="/signup"
+              element={
+                <ProtectSignup>
+                  <SignupForm />
+                </ProtectSignup>
+              }
+            />
 
-                        <Route path="/signup" element={<ProtectSignup>
-                            <SignupForm/>
-                            </ProtectSignup>} />
+            <Route
+              path="/profile/:profileId"
+              element={
+                <ProtectedRoute>
+                  <ProfilePage />
+                </ProtectedRoute>
+              }
+            />
 
-           
+            <Route
+              path="/settings"
+              element={
+                <ProtectedRoute>
+                  <ProfilePage />
+                </ProtectedRoute>
+              }
+            />
 
-<Route path="/profile/:profileId" element={
-            <ProtectedRoute>
-            <ProfilePage/>
-            </ProtectedRoute>} />
-
-            <Route path="/settings" element={
-            <ProtectedRoute>
-            <ProfilePage/>
-            </ProtectedRoute>} />
-
-         <Route path="*" element={<ErrorPage />}/>
-
-         </Routes>
-         
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
         </Box>
-        </CssBaseline>
+      </CssBaseline>
     </ThemeProvider>
   );
 }
