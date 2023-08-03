0be85593bb3c731e1486823e9652a8fb44defed7
Bianca Simon <biancasimon2112@gmail.com>
2023-08-02 20:44:56 -0500

    Change browser router to hash router

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { UserContextProvider } from './utils/UserContext';
-import { BrowserRouter as Router } from "react-router-dom"
+// import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
      <UserContextProvider>
-    <Router>
+    <HashRouter>
     <App />
-    </Router>
+    </HashRouter>
     </UserContextProvider>
      
   </React.StrictMode>
